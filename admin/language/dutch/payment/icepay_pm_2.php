96f46b56eb27b30344a47235a9b0b1aa2d941b26
Ricardo Jacobs <ricardozegt@gmail.com>
2015-09-24 16:23:48 +0200

    Pushing required payment method handlers.

---
+++ admin/language/dutch/payment/icepay_pm_2.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @package       ICEPAY Payment Module for OpenCart
+ * @author        Ricardo Jacobs <ricardo.jacobs@icepay.com>
+ * @copyright     (c) 2015 ICEPAY. All rights reserved.
+ * @version       2.0.5, September 2015
+ * @license       BSD 2 License, see https://github.com/icepay/OpenCart/blob/master/LICENSE
+ */
+
+$_['heading_title'] = "icepay_pm_2 <script>jQuery(\"td:contains('icepay_pm_2')\").parent('tr').hide();</script>";
---
+++ catalog/controller/payment/icepay_pm_2.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package       ICEPAY Payment Module for OpenCart
+ * @author        Ricardo Jacobs <ricardo.jacobs@icepay.com>
+ * @copyright     (c) 2015 ICEPAY. All rights reserved.
+ * @version       2.0.5, September 2015
+ * @license       BSD 2 License, see https://github.com/icepay/OpenCart/blob/master/LICENSE
+ */
+
+require_once(realpath(dirname(__FILE__)) . '/icepay_basic.php');
+
+class ControllerPaymentIcepayPm2 extends ControllerPaymentIcepayBasic
+{
+    protected $pmCode = '2';
+}
