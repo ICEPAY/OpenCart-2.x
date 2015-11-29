7b75552abeb0d1997e9d3e7de1dd604bd294f348
tsygankov <s.tsygankov@balticsolutions.lv>
2015-11-30 00:51:40 +0200

    Changed version information
    1) Version information removed from files header
    2) Current version set to 2.0.7

--- catalog/controller/payment/icepay_basic.php
+++ catalog/controller/payment/icepay_basic.php
@@ -4,11 +4,10 @@
  * @package       ICEPAY Payment Module for OpenCart
  * @author        Ricardo Jacobs <ricardo.jacobs@icepay.com>
  * @copyright     (c) 2015 ICEPAY. All rights reserved.
- * @version       2.0.6 September 2015
  * @license       BSD 2 License, see https://github.com/icepay/OpenCart/blob/master/LICENSE
  */
 
-define('ICEPAY_MODULE_VERSION', '2.0.5');
+define('ICEPAY_MODULE_VERSION', '2.0.7');
 
 class ControllerPaymentIcepayBasic extends Controller
 {
